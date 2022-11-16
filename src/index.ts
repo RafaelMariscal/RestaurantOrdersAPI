4368df756b7145fdaad9b0ea83f77086f4a06e8e
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-11-16 10:57:33 -0300

    Add: Categories Routes
    The api has now a create category route, and also a list categories route

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
+import { router } from "./router";
 
 const DOCKERPORT = 27017;
 mongoose.connect(`mongodb://localhost/${DOCKERPORT}`)
   .then(() => {
     console.log("Connected with mongoose.");
-    const app = express();
     const PORT = 3001;
+    const app = express();
+
+    app.use(express.json()); // needs to be before app.use(router)
+    app.use(router);
 
     app.listen(PORT, () => {
       console.log(`server is running on http://localhost:${PORT}`);
