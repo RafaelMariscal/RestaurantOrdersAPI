941366abb4bdec089105a8edf72bfc82ddeef386
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-11-20 23:11:53 -0300

    Add: Websocket by socketIo

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import path from "node:path";
+import http from "node:http";
 import express from "express";
 import mongoose from "mongoose";
+import { Server } from "socket.io";
 
 import { router } from "./router";
 
 const DOCKERPORT = 27017;
 const PORT = 3001;
 
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
 mongoose.connect(`mongodb://localhost/${DOCKERPORT}`)
   .then(() => {
     console.log("Connected with mongoose.");
-    const app = express();
 
     app.use((req, res, next) => {
       res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
@@ -23,7 +28,7 @@ mongoose.connect(`mongodb://localhost/${DOCKERPORT}`)
     // this is use because, instead a dirpath, we need to provide the file itself
     app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`server is running on http://localhost:${PORT}`);
     });
   })
